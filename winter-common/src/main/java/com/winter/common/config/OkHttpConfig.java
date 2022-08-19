3f11e7086508301fe22e5456a00de345c7dcdf08
Damien <damien2016@126.com>
2022-08-19 11:13:01 +0800

    feat: okhttp

---
+++ winter-common/src/main/java/com/winter/common/config/OkHttpConfig.java
@@ -0,0 +1,123 @@
+package com.winter.common.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * OkHttp 配置
+ * <p>
+ * </p>
+ *
+ * @author Damien
+ * @description
+ * @create 2022/8/19 9:59
+ */
+@Configuration
+@ConfigurationProperties(prefix = "okhttp")
+@Getter
+@Setter
+public class OkHttpConfig {
+
+    /**
+     * 连接时间
+     */
+    private Long connectTimeout = 10L;
+
+    /**
+     * 读时间
+     */
+    private Long readTimeout = 10L;
+
+    /**
+     * 写时间
+     */
+    private Long writeTimeout = 10L;
+
+    /**
+     * 最大连接数
+     */
+    private Integer maxIdleConnections = 200;
+
+    /**
+     * 保持active时间
+     */
+    private Long keepAliveDuration = 5L;
+
+    @Bean
+    public X509TrustManager x509TrustManager() {
+        return new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+            }
+
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+        };
+
+    }
+
+    @Bean
+    public SSLSocketFactory sslSocketFactory() {
+        try {
+            //信任任何链接
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, new TrustManager[]{x509TrustManager()}, new SecureRandom());
+            return sslContext.getSocketFactory();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Create a new connection pool with tuning parameters appropriate for a single-user application.
+     * The tuning parameters in this pool are subject to change in future OkHttp releases. Currently
+     */
+    @Bean
+    public ConnectionPool pool() {
+        return new ConnectionPool(maxIdleConnections, keepAliveDuration, TimeUnit.MINUTES);
+    }
+
+    @Bean
+    public OkHttpClient okHttpClient() {
+        return new OkHttpClient.Builder()
+                .sslSocketFactory(sslSocketFactory(), x509TrustManager())
+                .retryOnConnectionFailure(false)//是否开启缓存
+                .connectionPool(pool())//连接池
+                .connectTimeout(connectTimeout, TimeUnit.SECONDS)
+                .readTimeout(readTimeout, TimeUnit.SECONDS)
+                .writeTimeout(writeTimeout, TimeUnit.SECONDS)
+                .hostnameVerifier((hostname, session) -> true)
+                // 设置代理
+//            	.proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 8888)))
+                // 拦截器
+//                .addInterceptor()
+                .build();
+    }
+
+}
+
