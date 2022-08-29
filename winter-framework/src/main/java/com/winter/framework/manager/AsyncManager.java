ec92e76f6f79fd5de6eaf7f070bfe083e6a61341
Damien <damien2016@126.com>
2022-08-29 17:31:06 +0800

    feat: init AsyncManager executor

--- winter-framework/src/main/java/com/winter/framework/manager/AsyncManager.java
+++ winter-framework/src/main/java/com/winter/framework/manager/AsyncManager.java
@@ -1,18 +1,18 @@
 package com.winter.framework.manager;
 
-import java.util.TimerTask;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 import com.winter.common.utils.Threads;
 import com.winter.common.utils.spring.SpringUtils;
 
+import java.util.TimerTask;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 /**
  * 异步任务管理器
- * 
+ *
  * @author winter
  */
-public class AsyncManager
-{
+public class AsyncManager {
     /**
      * 操作延迟10毫秒
      */
@@ -21,35 +21,34 @@ public class AsyncManager
     /**
      * 异步操作任务调度线程池
      */
-    private ScheduledExecutorService executor = SpringUtils.getBean("scheduledExecutorService");
+    private ScheduledExecutorService executor;
 
     /**
      * 单例模式
      */
-    private AsyncManager(){}
+    private AsyncManager() {
+    }
 
     private static AsyncManager me = new AsyncManager();
 
-    public static AsyncManager me()
-    {
+    public static AsyncManager me() {
+        me.executor = SpringUtils.getBean("scheduledExecutorService");
         return me;
     }
 
     /**
      * 执行任务
-     * 
+     *
      * @param task 任务
      */
-    public void execute(TimerTask task)
-    {
+    public void execute(TimerTask task) {
         executor.schedule(task, OPERATE_DELAY_TIME, TimeUnit.MILLISECONDS);
     }
 
     /**
      * 停止任务线程池
      */
-    public void shutdown()
-    {
+    public void shutdown() {
         Threads.shutdownAndAwaitTermination(executor);
     }
 }
