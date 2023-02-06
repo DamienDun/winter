af60e32b050a5895c58cf57a94d7b4ef4cc2174a
Damien <damien2016@126.com>
2023-02-06 14:29:38 +0800

    feat: Date serialize support annotation JsonFormat

--- winter-common/src/main/java/com/winter/common/utils/json/DateSerializer.java
+++ winter-common/src/main/java/com/winter/common/utils/json/DateSerializer.java
@@ -1,12 +1,19 @@
 package com.winter.common.utils.json;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.BeanProperty;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.introspect.AnnotationMap;
+import com.fasterxml.jackson.databind.ser.ContextualSerializer;
 import com.winter.common.utils.DateUtils;
+import com.winter.common.utils.StringUtils;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.Objects;
 
 /**
  * Date序列化
@@ -17,13 +24,28 @@ import java.util.Date;
  * @description
  * @create 2022/9/19 14:10
  */
-public class DateSerializer extends JsonSerializer<Date> {
+public class DateSerializer extends JsonSerializer<Date> implements ContextualSerializer {
 
     public final static DateSerializer INSTANCE = new DateSerializer();
 
+    private String format = DateUtils.YYYY_MM_DD_HH_MM_SS;
+
     @Override
     public void serialize(Date value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-        String formattedDate = DateUtils.parseDateToStr(DateUtils.YYYY_MM_DD_HH_MM_SS, value);
+        String formattedDate = DateUtils.parseDateToStr(format, value);
         gen.writeString(formattedDate);
+    }
+
+    @Override
+    public JsonSerializer<?> createContextual(SerializerProvider prov, BeanProperty property) throws JsonMappingException {
+        if (Objects.isNull(property)) {
+            return this;
+        }
+        AnnotationMap anno = property.getMember().getAllAnnotations();
+        JsonFormat jsonFormat = anno.get(JsonFormat.class);
+        if (jsonFormat != null && StringUtils.isNotEmpty(jsonFormat.pattern())) {
+            format = jsonFormat.pattern();
+        }
+        return this;
     }
 }
--- winter-common/src/main/java/com/winter/common/utils/json/LocalDateTimeSerializer.java
+++ winter-common/src/main/java/com/winter/common/utils/json/LocalDateTimeSerializer.java
@@ -1,12 +1,19 @@
 package com.winter.common.utils.json;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.BeanProperty;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.introspect.AnnotationMap;
+import com.fasterxml.jackson.databind.ser.ContextualSerializer;
 import com.winter.common.utils.DateUtils;
+import com.winter.common.utils.StringUtils;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 /**
  * LocalDateTime 序列化
@@ -17,13 +24,28 @@ import java.time.LocalDateTime;
  * @description
  * @create 2022/9/19 14:25
  */
-public class LocalDateTimeSerializer extends JsonSerializer<LocalDateTime> {
+public class LocalDateTimeSerializer extends JsonSerializer<LocalDateTime> implements ContextualSerializer {
 
     public final static LocalDateTimeSerializer INSTANCE = new LocalDateTimeSerializer();
 
+    private String format = DateUtils.YYYY_MM_DD_HH_MM_SS;
+
     @Override
     public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-        String formattedDate = DateUtils.parseDateToStr(DateUtils.YYYY_MM_DD_HH_MM_SS, DateUtils.from(value));
+        String formattedDate = DateUtils.parseDateToStr(format, DateUtils.from(value));
         gen.writeString(formattedDate);
+    }
+
+    @Override
+    public JsonSerializer<?> createContextual(SerializerProvider prov, BeanProperty property) throws JsonMappingException {
+        if (Objects.isNull(property)) {
+            return this;
+        }
+        AnnotationMap anno = property.getMember().getAllAnnotations();
+        JsonFormat jsonFormat = anno.get(JsonFormat.class);
+        if (jsonFormat != null && StringUtils.isNotEmpty(jsonFormat.pattern())) {
+            format = jsonFormat.pattern();
+        }
+        return this;
     }
 }
