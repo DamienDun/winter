929889e7a846d95c243f2541e57152f1c8be28e5
Damien <damien2016@126.com>
2023-03-09 17:05:13 +0800

    feat: 修改业务类型常量值

--- winter-common/src/main/java/com/winter/common/constant/BusinessType.java
+++ winter-common/src/main/java/com/winter/common/constant/BusinessType.java
@@ -9,50 +9,50 @@ public interface BusinessType {
     /**
      * 其它
      */
-    int OTHER = 1;
+    int OTHER = 0;
 
     /**
      * 新增
      */
-    int INSERT = 2;
+    int INSERT = 1;
 
     /**
      * 修改
      */
-    int UPDATE = 3;
+    int UPDATE = 2;
 
     /**
      * 删除
      */
-    int DELETE = 4;
+    int DELETE = 3;
 
     /**
      * 授权
      */
-    int GRANT = 5;
+    int GRANT = 4;
 
     /**
      * 导出
      */
-    int EXPORT = 6;
+    int EXPORT = 5;
 
     /**
      * 导入
      */
-    int IMPORT = 7;
+    int IMPORT = 6;
 
     /**
      * 强退
      */
-    int FORCE = 8;
+    int FORCE = 7;
 
     /**
      * 生成代码
      */
-    int GENCODE = 9;
+    int GENCODE = 8;
 
     /**
      * 清空数据
      */
-    int CLEAN = 10;
+    int CLEAN = 9;
 }
