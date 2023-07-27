3274ecb20d31f8f73555e6d9df40ba22098c8ee4
Damien <damien2016@126.com>
2023-07-27 15:01:21 +0800

    fix: 字段找不到问题

--- winter-common/src/main/java/com/winter/common/utils/json/DateSerializer.java
+++ winter-common/src/main/java/com/winter/common/utils/json/DateSerializer.java
@@ -9,12 +9,13 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.ContextualSerializer;
 import com.winter.common.utils.DateUtils;
 import com.winter.common.utils.StringUtils;
-import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
-import java.util.Objects;
+import java.util.List;
 
 /**
  * Date序列化
@@ -25,7 +26,6 @@ import java.util.Objects;
  * @description
  * @create 2022/9/19 14:10
  */
-@Slf4j
 public class DateSerializer extends JsonSerializer<Date> implements ContextualSerializer {
 
     public final static DateSerializer INSTANCE = new DateSerializer();
@@ -37,15 +37,16 @@ public class DateSerializer extends JsonSerializer<Date> implements ContextualSe
         Class<?> aClass = gen.getCurrentValue().getClass();
         // 获取字段名
         String currentName = gen.getOutputContext().getCurrentName();
-        try {
-            // 获取字段
-            Field declaredField = aClass.getDeclaredField(currentName);
-            JsonFormat jsonFormat = declaredField.getAnnotation(JsonFormat.class);
-            if (jsonFormat != null && StringUtils.isNotEmpty(jsonFormat.pattern())) {
-                format = jsonFormat.pattern();
+        List<Field> fieldList = new ArrayList<>();
+        fieldList.addAll(Arrays.asList(aClass.getFields()));
+        fieldList.addAll(Arrays.asList(aClass.getDeclaredFields()));
+        for (Field field : fieldList) {
+            if (field.getName().equals(currentName)) {
+                JsonFormat jsonFormat = field.getAnnotation(JsonFormat.class);
+                if (jsonFormat != null && StringUtils.isNotEmpty(jsonFormat.pattern())) {
+                    format = jsonFormat.pattern();
+                }
             }
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
         }
         String formattedDate = DateUtils.parseDateToStr(format, value);
         gen.writeString(formattedDate);
@@ -53,9 +54,6 @@ public class DateSerializer extends JsonSerializer<Date> implements ContextualSe
 
     @Override
     public JsonSerializer<?> createContextual(SerializerProvider prov, BeanProperty property) throws JsonMappingException {
-        if (Objects.isNull(property)) {
-            return this;
-        }
         return this;
     }
 }
--- winter-common/src/main/java/com/winter/common/utils/json/LocalDateTimeSerializer.java
+++ winter-common/src/main/java/com/winter/common/utils/json/LocalDateTimeSerializer.java
@@ -9,12 +9,13 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.ContextualSerializer;
 import com.winter.common.utils.DateUtils;
 import com.winter.common.utils.StringUtils;
-import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.time.LocalDateTime;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * LocalDateTime 序列化
@@ -25,7 +26,6 @@ import java.util.Objects;
  * @description
  * @create 2022/9/19 14:25
  */
-@Slf4j
 public class LocalDateTimeSerializer extends JsonSerializer<LocalDateTime> implements ContextualSerializer {
 
     public final static LocalDateTimeSerializer INSTANCE = new LocalDateTimeSerializer();
@@ -37,15 +37,16 @@ public class LocalDateTimeSerializer extends JsonSerializer<LocalDateTime> imple
         Class<?> aClass = gen.getCurrentValue().getClass();
         // 获取字段名
         String currentName = gen.getOutputContext().getCurrentName();
-        try {
-            // 获取字段
-            Field declaredField = aClass.getDeclaredField(currentName);
-            JsonFormat jsonFormat = declaredField.getAnnotation(JsonFormat.class);
-            if (jsonFormat != null && StringUtils.isNotEmpty(jsonFormat.pattern())) {
-                format = jsonFormat.pattern();
+        List<Field> fieldList = new ArrayList<>();
+        fieldList.addAll(Arrays.asList(aClass.getFields()));
+        fieldList.addAll(Arrays.asList(aClass.getDeclaredFields()));
+        for (Field field : fieldList) {
+            if (field.getName().equals(currentName)) {
+                JsonFormat jsonFormat = field.getAnnotation(JsonFormat.class);
+                if (jsonFormat != null && StringUtils.isNotEmpty(jsonFormat.pattern())) {
+                    format = jsonFormat.pattern();
+                }
             }
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
         }
         String formattedDate = DateUtils.parseDateToStr(format, DateUtils.from(value));
         gen.writeString(formattedDate);
@@ -53,9 +54,6 @@ public class LocalDateTimeSerializer extends JsonSerializer<LocalDateTime> imple
 
     @Override
     public JsonSerializer<?> createContextual(SerializerProvider prov, BeanProperty property) throws JsonMappingException {
-        if (Objects.isNull(property)) {
-            return this;
-        }
         return this;
     }
 }
