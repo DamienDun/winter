a346f344a74e146ada2b325f4de30a4e2da2d836
Damien <damien2016@126.com>
2023-12-22 09:40:32 +0800

    feat: 刷新token改为调接口立马刷新

--- winter-framework/src/main/java/com/winter/framework/security/filter/JwtAuthenticationTokenFilter.java
+++ winter-framework/src/main/java/com/winter/framework/security/filter/JwtAuthenticationTokenFilter.java
@@ -1,24 +1,25 @@
 package com.winter.framework.security.filter;
 
-import java.io.IOException;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import com.winter.common.core.domain.model.LoginUser;
+import com.winter.common.utils.SecurityUtils;
+import com.winter.common.utils.StringUtils;
+import com.winter.framework.web.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import com.winter.common.core.domain.model.LoginUser;
-import com.winter.common.utils.SecurityUtils;
-import com.winter.common.utils.StringUtils;
-import com.winter.framework.web.service.TokenService;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * token过滤器 验证token有效性
- * 
+ *
  * @author winter
  */
 @Component
@@ -34,7 +35,7 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter
         LoginUser loginUser = tokenService.getLoginUser(request);
         if (StringUtils.isNotNull(loginUser) && StringUtils.isNull(SecurityUtils.getAuthentication()))
         {
-            tokenService.verifyToken(loginUser);
+            tokenService.refreshToken(loginUser);
             UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginUser, null, loginUser.getAuthorities());
             authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
             SecurityContextHolder.getContext().setAuthentication(authenticationToken);
