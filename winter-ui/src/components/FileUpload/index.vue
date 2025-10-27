6635196deb9a6c1c83467d0ac8896e62e99e75ac
Damien <damien2016@126.com>
2025-10-27 08:22:26 +0800

    文件上传组件新增类型

--- winter-ui/src/components/FileUpload/index.vue
+++ winter-ui/src/components/FileUpload/index.vue
@@ -60,7 +60,7 @@ export default {
     // 文件类型, 例如['png', 'jpg', 'jpeg']
     fileType: {
       type: Array,
-      default: () => ["doc", "xls", "ppt", "txt", "pdf"],
+      default: () => ["doc", "docx", "xls", "xlsx", "ppt", "pptx", "txt", "pdf"],
     },
     // 是否显示提示
     isShowTip: {
